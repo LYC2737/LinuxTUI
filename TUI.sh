d43cc30973e38174eaa2f0b51a43fc69cb928fb0
testUser <test@example.com>
2026-09-09 11:24:49 +0900

    feat: menu 1번 system info 완료

--- TUI.sh
+++ TUI.sh
@@ -14,9 +14,38 @@ while true; do
         "3" "Service Info" \
         "4" "Exit" 3>&1 1>&2 2>&3)
 
-
 if [ $? -ne 0 ]; then
 	exit 1
 fi
+
+# CASE 구문 사용하여 메뉴 선택시 분기 실행
+case $MENU in
+	"1")	# 메뉴 1번, System Info 
+		echo "=== Host Name ===" > $TEMP
+		echo "HostName: $(hostname)" >> $TEMP
+		printf "\n\n" >> $TEMP
+
+		echo "=== IP info ===" >> $TEMP
+		ip  -4 -o a sh |awk '{print "Interface: " $2 " --> IP: " $4}' |column -t >> $TEMP
+		printf "\n\n" >> $TEMP
+
+		echo "=== Routing Table ===" >> $TEMP
+		ip route |column -t >> $TEMP
+		printf "\n\n" >> $TEMP
+
+		echo "=== df info ===" >> $TEMP
+		df -TPh >> $TEMP
+
+		# $TEMP로 받은 내용 화면에 출력하기
+		whiptail --title="System Info" --scrolltext --textbox "$TEMP" 30 100	
+		;;
+	"2")
+		;;
+
+	"3")
+		;;
+	"4")
+		;;
+esac
 
 done 
